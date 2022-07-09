f29d913348ed2313a39592133a111408a90bcb95
zendu <duzhenquan@eastmoney.com>
2022-07-09 11:11:16 +0800

    update

--- pipline.sh
+++ pipline.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-
+file_name="src/index.ts"
 msg="update"
+
 if [[ ! -z $1 ]];then
     msg=$1
 fi
+
+
+tf="console.log('%c【PUBLISH TIME】-> ${fmt_date}','color:#5FD068;margin-left: 20%;padding: 20px;');"
+gsed -i -e '$s/console.log(.*);/'"$tf"'/' ${file_name}
+
 
 git add .
 git commit -m "$msg"
