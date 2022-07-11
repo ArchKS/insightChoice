c9f6e8874b225558556594bdc6d320e3ef7923a6
zendu <duzhenquan@eastmoney.com>
2022-07-11 09:27:39 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -25,4 +25,4 @@ root.render(
 
 
 
-console.log('%c【UPDATE TIME】-> 2022-07-11 09:25:33','color:#5FD068;margin-left: 20%;padding: 20px;');
+console.log('%c【UPDATE TIME】-> 2022-07-11 09:27:39','color:#5FD068;margin-left: 20%;padding: 20px;');
