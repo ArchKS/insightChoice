1bcf55a791f600dad0e44a6e4291473aa182edee
zendu <duzhenquan@eastmoney.com>
2022-07-11 09:01:20 +0800

    add diff color

--- src/index.jsx
+++ src/index.jsx
@@ -25,4 +25,4 @@ root.render(
 
 
 
-console.log('%c【PUBLISH TIME】-> 2022-07-11 09:00:25','color:#5FD068;margin-left: 20%;padding: 20px;');
+console.log('%c【PUBLISH TIME】-> 2022-07-11 09:01:20','color:#5FD068;margin-left: 20%;padding: 20px;');
